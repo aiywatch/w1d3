4e907cfb84bd6c438f6c1b268eae1428bdb82f27
aiywatch <aiycp35@gmail.com>
2016-06-22 19:39:04 +0000

    state

---
+++ state and cities/state_info.rb
@@ -0,0 +1,58 @@
+$states = {
+  OR: 'Oregon',
+  FL: 'Florida',
+  CA: 'California',
+  NY: 'New York',
+  MI: 'Michigan'
+}
+
+$cities = {
+  OR: ['aaaa', 'bbbb'],
+  FL: ['ccccc'],
+  CA: ['ffff','gggggg','hhhhhh'],
+  NY: []
+}
+$cities.default = 0
+
+$taxes = {
+  OR: 7.5,
+  FL: 10,
+  CA: 12,
+  NY: 50
+}
+$taxes.default = 80
+
+$states[:BC] = 'British Columbia'
+$states[:TO] = 'Toronto'
+
+p $states
+
+def describe_state (code)
+  count_cities = $cities[code.to_sym].count
+  # part_two = '#{count_cities} major cities:'
+  ans = "#{code} is for #{$states[code.to_sym]}. It has "
+
+  if count_cities == 0
+    ans << "no city"
+  elsif count_cities == 1
+    ans << "#{count_cities} major city: #{$cities[code.to_sym].join(', ')}"
+  else 
+    ans << "#{count_cities} major cities: #{$cities[code.to_sym].join(', ')}"
+  end
+end
+
+p describe_state ('CA')
+p describe_state ('FL')
+p describe_state ('NY')
+
+def calculate_tax(code, dollor)
+  $taxes[code.to_sym] * dollor.to_f
+end
+
+p calculate_tax('OR', 900)
+
+def find_state_for_city(city)
+  ans = $cities.select { |k, v| v.include?(city)}.keys.join()
+end
+
+p find_state_for_city('aaaa')
